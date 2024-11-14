56df4db523fef2b6167311f0b3c32269aef39863
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-14 14:11:09 +0900

    react-router-domを導入

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Calendar from "./pages/Calendar/calendar.jsx";
+import Calendar from "./pages/Calendar/Calendar.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "not_available",
+    element: <h1>ただいま離席中です</h1>,
+  },
+  {
+    path: "/calendar",
+    element: <Calendar />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Calendar />
+    <RouterProvider router={router} />
   </StrictMode>
 );
