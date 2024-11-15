582db5d97cbccd6a7d5e45350773ff47ee6fa4ca
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-15 09:31:50 +0900

    calendarページにダミーイベントを追加

--- frontend/src/components/CalendarBody/CalendarBody.jsx
+++ frontend/src/components/CalendarBody/CalendarBody.jsx
@@ -6,9 +6,37 @@ import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClic
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 function CalendarBody() {
+  const eventList = [
+    {
+      eventId: "1",
+      title: "池袋でlunch",
+      // date: "2024-11-14",
+      place: "池袋",
+      start: "2024-11-14",
+      end: "2024-11-16",
+      startTime: "09",
+      endTime: "14",
+      detail: "池袋でお昼にジンギスカンを食べませんか?",
+      requests: [],
+      eventOwner: "oguma",
+    },
+    {
+      eventId: "2",
+      title: "新宿でlunch",
+      date: "2024-11-15",
+      place: "新宿",
+      startTime: "11",
+      endTime: "17",
+      detail: "新宿でお昼にサムギョプサルを食べませんか?",
+      requests: [],
+      eventOwner: "oguma",
+    },
+  ];
+
   const handleDateClick = (arg) => {
     alert(arg.dateStr);
   };
+
   return (
     <>
       <Frame>
@@ -16,10 +44,7 @@ function CalendarBody() {
           aspectRatio={1}
           plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
           initialView="dayGridMonth"
-          events={[
-            { title: "池袋でお昼ご飯!!", date: "2024-11-14" },
-            { title: "新宿で夜ご飯", date: "2024-11-14" },
-          ]}
+          events={eventList}
           dateClick={handleDateClick}
           headerToolbar={{
             start: "title",
@@ -37,6 +62,9 @@ const Frame = styled.div`
   width: 95%;
   --fc-neutral-text-color: ${(props) => props.theme.palette.primary.main};
   --fc-border-color: ${(props) => props.theme.palette.primary.main};
+  --fc-small-font-size: 7px;
+  --fc-event-bg-color: ${(props) => props.theme.palette.primary.main};
+  --fc-event-border-color: ${(props) => props.theme.palette.primary.main};
 
   a {
     color: ${(props) => props.theme.palette.primary.main};
